92dd8551e08f07b4d5f673808e6e905755cd2bef
linyujing199488 <67212190+linyujing199488@users.noreply.github.com>
2020-11-25 10:22:17 +0800

    Update seq length.py

--- seq length.py
+++ seq length.py
@@ -4,27 +4,33 @@ def file_input():
 	fi = input("请输入文件的名称:")
 	f = open(fi,"r",encoding="utf-8").read()
 	return f
-#get sequence length
-def get_len(f):
+	
+def get_seq(f):
 	m = []
-	id = []
-	dict = {}
 	line = f.split('\n')
-	re_s = r'[^>]'
+	re_s=r'[^>]'
 	while '' in line:
 		line.remove('')
 		for t in line:
 			n = re.match(re_s,t)
 			if n:
 				m.append(len(t))
+		return m
+
+def get_id(f):
+	id = []
+	line = f.split('\n')
+	while '' in line:
+		line.remove('')
 		for t in line:
 			if '>' in t:
-				id.append(t)		
-		for i in range(len(id)):
-			dict[id[i]] = m[i]
-			for key,value in dict.items():
-				return "{}:{}".format(key,value)
-#output file
+				id.append(t)
+		return id
+			
+def list_dict(list1,list2):
+	dic = dict(map(lambda x,y:[x,y], list1,list2))
+	return dic
+
 def file_output(dict):
 	with open("seq.txt","w") as fo:
 		fo.write(str(dict))
@@ -32,7 +38,9 @@ def file_output(dict):
 		
 def main():
 	f = file_input()
-	dict = get_len(f)
-	file_output(dict)
+	ID = get_id(f)
+	Seq = get_seq(f)
+	dic = list_dict(ID,Seq)	
+	file_output(dic)
 
 main()
